05d13b6b063e0897504bdc3e8ebf057c7bd4bb7a
pnpyrlrm <dv56026@gmail.com>
2024-01-15 20:27:00 +0200

    /

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,46 @@
-import '../../script/test'
+class Slider {
+  static #content = null
+  static #left = null
+  static #right = null
 
-console.log('container')
+  static #count = 1
+  static #max = null
+
+  static init = () => {
+    this.#content = document.querySelector(
+      'slider__content',
+    )
+    this.#left = document.querySelector(
+      'slider__button--left',
+    )
+    this.#right = document.querySelector(
+      'slider__button--right',
+    )
+    this.#max = this.#content.childElementCount
+    this.#left.onclick = () => this.#slide('left')
+    this.#right.onclick = () => this.#slide('right')
+  }
+
+  static #slide = (side) => {
+    const offsetWidth = this.#content.offsetWidth
+    const scrollLeft = this.#content.scrollLeft
+    const scrollWidht = this.#content.scrollWidht
+
+    let scroll = 0
+
+    if (side === 'left') {
+      scroll = -100
+    }
+    if (side === 'right') {
+      scroll = 100
+    }
+
+    this.#content.scrollTo({
+      top: 0,
+      left: scroll,
+      behavior: 'smooth',
+    })
+  }
+}
+
+Slider.init()
